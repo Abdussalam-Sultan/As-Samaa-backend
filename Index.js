9e0b257d24107eacbdf76da093aea3ed7edf2bcd
Abdussalam Sultan <108432183+Abdussalam-Sultan@users.noreply.github.com>
2026-04-02 00:20:07 +0100

    Add request logging middleware and root endpoint

--- Index.js
+++ Index.js
@@ -31,6 +31,14 @@ const storage = multer.diskStorage({
 //     },
 //   ]);
 // });
+app.use((req, res, next) => {
+  console.log("REQUEST:", req.method, req.url);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "API is running" });
+});
 app.post("/api/search",multer({ storage }).single('audio'), async (req, res) => {
   //const query = req.body.query;
   const query = await transcribeFile(req.file.path);
